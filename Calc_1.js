49c9cfd07ed4a669fba0a41367d7f66ff23fd2f6
git-prk <praneeta.katkar@gmail.com>
2024-02-06 21:09:53 +0530

    added

---
+++ Calc_1.js
@@ -0,0 +1,49 @@
+let a = 10
+console.log(a)
+a = 100
+console.log(a)
+
+const h = 1000
+console.log(h)
+//h = 100
+
+// program 2
+
+let a1 = 10 
+let b1 = 5
+
+console.log(a1+b1)
+console.log(a1-b1)
+console.log(a1*b1)
+console.log(a1/b1)
+console.log(a1%b1)
+
+let x = 8
+let y = 4
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+
+// 10 pairs --- 50 lines 
+
+// function - keyword
+// calculator - function name 
+// () - parenthesis
+// x,y - parameter
+// {} - blocks
+// {---} --- statements
+// Calculator(23,4)
+// 23,4 --- arguments
+
+function Calculator(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+Calculator(4,2)
+Calculator(14,7)
