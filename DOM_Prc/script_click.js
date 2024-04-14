9ab8145640d206d2877d446d082fdb6f82e86d3c
git-prk <praneeta.katkar@gmail.com>
2024-04-14 21:49:05 +0530

    add

---
+++ DOM_Prc/script_click.js
@@ -0,0 +1,14 @@
+let headOne = document.querySelector('h1')
+headOne.addEventListener('click',function(){
+    headOne.style.color = "red"
+})
+
+
+// info = {
+//     fullName:"chinmay deshpande",
+//     parent:{
+//         father:"shirish",
+//         mother:"kanchan"
+//     }
+// }
+// info.parent.mother = "kanchan deshpande"
