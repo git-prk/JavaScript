e7da3406d6e519ef880974b9d5d928a1fb50833b
git-prk <praneeta.katkar@gmail.com>
2024-04-14 21:46:25 +0530

    add ajax

---
+++ Ajax/apiDataAll.js
@@ -0,0 +1,26 @@
+// add button on UI
+
+
+let addusers = document.querySelector('button')
+
+
+//Data to be displayed for all the elements
+addusers.addEventListener('click', function () {
+    fetch(`https://reqres.in/api/users?page=2`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (response) {
+            console.log(response.data)
+            response.data.forEach(el => {
+                document.write(`<h1>${el.first_name}</h1>`)
+                document.write(`<h1>${el.last_name}</h1>`)
+                document.write(`<p>${el.email}</p>`)
+                document.write(`<h1>${el.id}<h1>`)
+                document.write(`<img src=${el.avatar}>`)
+
+            });
+
+
+        })
+})
